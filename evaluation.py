fc4b84db165b13e49f9fec1d1d34e348c128817d
ftyQAQ <52941817+ftyQAQ@users.noreply.github.com>
2021-08-05 08:49:18 +0800

    Add files via upload

---
+++ evaluation.py
@@ -0,0 +1,107 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import numpy as np
+import dxchange
+import os
+from shift_resnet import resnet18
+import tomopy
+import torch
+import matplotlib.pyplot as plt
+import cv2
+import torch.nn as nn
+from res_data import DatasetFromFolder
+from torch.utils.data import DataLoader
+from sklearn.metrics import mean_squared_error
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".tiff"])
+def red_stack_tiff(path):
+    # path0 = 'D:/pycharm/pycharm/py/resig/data/shapp3d_160/'
+    files = os.listdir(path)
+    prj = []
+    # prj0 = np.zeros((len(files), size, size))
+    for n,file in enumerate(files):
+        if is_image_file(file):
+            p = dxchange.read_tiff(path + file)
+            prj.append(p)
+    pr = np.array(prj)
+    return pr
+def nor_data(img):
+    mean_tmp = np.mean(img)
+    std_tmp = np.std(img)
+    img = (img - mean_tmp) / std_tmp
+    img = (img - img.min())/(img.max()-img.min())
+    return img
+def tonor_data(img):
+    img = (img-torch.min(img))/(torch.max(img)-torch.min(img))
+    return img
+
+def is_txt_file(filename):
+    return any(filename.endswith(extension) for extension in [".txt"])
+def get_args():
+    parser = argparse.ArgumentParser(description='Test the Net',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--test__path', type=str, default='/path/to/data/predict/')
+    parser.add_argument('--model_dir', type=str, default='/path/to/data/test/')
+    return parser.parse_args()
+if __name__ == "__main__":
+    opt = get_args()
+    for indx in indxs:
+        test__path = opt.test__path
+        for batch_size in range(182):
+            print('batch_size : ', batch_size)
+            test_set = DatasetFromFolder(test__path, test__path)
+            testing_data_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
+            device = "cuda"
+            model = resnet18()
+            model.to(device)
+            plt_loss_max = []
+            plt_loss = []
+            plt_mean_loss = []
+
+            ph_model = opt.model_dir
+            model.load_state_dict(torch.load(ph_model))
+            shifts = torch.tensor([])
+            mse = nn.MSELoss(reduction='sum')
+            mse.to(device)
+            avg_loss_mse_image = 0
+            loss_shift = torch.tensor([])
+            pred_shift = torch.tensor([])
+            for input_fx, input_mov, target in testing_data_loader:
+                loss_shift, pred_shift = loss_shift.to(device), pred_shift.to(device)
+                input_fx, input_mov = tonor_data(input_fx), tonor_data(input_mov)
+                input_fx = input_fx.to(device)
+                input_mov = input_mov.to(device)
+                target = target.to(device)
+                shift = model(input_fx, input_mov)
+                loss_mse_image = mse(shift, target)
+                avg_loss_mse_image += loss_mse_image
+                a = shift - target
+                loss_shift = torch.cat([loss_shift, shift - target], dim=0)
+                pred_shift = torch.cat([pred_shift, shift], dim=0)
+
+            letest = np.ceil(len(testing_data_loader.dataset)) * 2.
+            avg_loss_mse_image = avg_loss_mse_image / letest
+            loss_shift = loss_shift.to('cpu').detach().numpy()
+            pred_shift = pred_shift.to('cpu').detach().numpy()
+            loss_shift_max = np.max(np.fabs(loss_shift))
+            loss_shift_mean = np.mean(np.fabs(loss_shift))
+            print('batch_size : ', batch_size,
+                  "item = %s  avg_loss_mse_image =  %f  loss_max = %f , mean_loss = %f" % (
+                      i, avg_loss_mse_image.item(), loss_shift_max, loss_shift_mean))
+            plt_mean_loss.append(loss_shift_mean)
+            plt_loss_max.append(loss_shift_max)
+            plt_loss.append(avg_loss_mse_image.item())
+        item_loss = np.argmin(plt_loss)
+        item_max_loss = np.argmin(plt_loss_max)
+        item_mean = np.argmin(plt_mean_loss)
+        print('item', item_loss, 'item_loss_max', plt_loss_max[item_loss], 'item_mse', plt_loss[item_loss],
+              'item_mean',
+              plt_mean_loss[item_loss])
+
+
+
+
--- train.py
+++ train.py
@@ -24,75 +24,45 @@ def nor_data(img):
 def tonor_data(img):
     img = (img-torch.min(img))/(torch.max(img)-torch.min(img))
     return img
-def _get_tomolearn_kwargs():
-    return {
-        'learning_rate': ,
-        'num_steps': ,
-        'display_step': ,
-        'train_batch_size':  ,
-        'test_batch_size':,
-        'nf': 'vm2',
-        'doc_path': '',
-        'test_path':'',
-        # 'predict_path':'D:/res_ture/predict/',
-        # 'vol_size':,
-        'weights_init': False,
-        'shuffle':True,
-        # 'lanmd':40,
-        'phat_txt':'',
-        'phatest_txt':'',
-        'model_dir':''
 
-    }
-def make_shift(input_movs,shifts,vol_size):
-    input_movs = input_movs.to('cpu').detach().numpy()
-    input_movs = input_movs.reshape(len(input_movs),vol_size,vol_size)
-    shifts = shifts.to('cpu').detach().numpy()
-    a = np.zeros((len(input_movs), vol_size, vol_size))
-    for i, (input_mov, shift) in enumerate(zip(input_movs, shifts)):
-        M = np.float32([[1, 0, -shift[0]], [0, 1, -shift[1]]])
-        img_s1 = cv2.warpAffine(input_mov, M, (vol_size, vol_size), borderValue=0).astype(np.float32)
-        a[i] = img_s1
-    a = a.reshape(len(a),1,vol_size,vol_size)
-    a_te = torch.tensor(a).type(torch.FloatTensor)
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the Net ',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--train_dataroot', type=str, default='/path/to/data/train/')
+    parser.add_argument('--test_dataroot', type=str, default='/path/to/data/test/')
+    parser.add_argument('--train_batch_size', type=int, default=8)
+    parser.add_argument('--test_batch_size', type=int, default=8)
+    parser.add_argument('--num_steps', type=int, default=44444)
+    parser.add_argument('--display_step', type=int, default=5)
+    parser.add_argument('--learning_rate', type=float, default=0.0001)
+    parser.add_argument('--model_dir', type=str, default='/path/to/your/model/')
+    parser.add_argument('--weights_init', type=str, default='False')
+    parser.add_argument('--shuffle', type=str, default='True')
+    return parser.parse_args()
 
-
-
-    return a_te
-def train(**kwargs):
-    cnn_kwargs = ['learning_rate', 'num_steps', 'display_step','train_batch_size', 'test_batch_size','nf',
-                  'doc_path','test_path','vol_size', 'weights_init','shuffle','lanmd','phat_txt','phatest_txt','model_dir']
-    kwargs_defaults = _get_tomolearn_kwargs()
-    for kw in cnn_kwargs:
-        kwargs.setdefault(kw, kwargs_defaults[kw])
+def train():
+    opt = get_args()
     device = "cuda"
-    train__path = kwargs['doc_path']
-    test__path = kwargs['test_path']
-    model_dir = kwargs['model_dir']
-    phat_txt = kwargs['phat_txt']
-    phatest_txt = kwargs['phatest_txt']
-    if not os.path.exists(phat_txt):
-        os.makedirs(phat_txt)
+    train__path = opt.train_dataroot
+    test__path = opt.test_dataroot
+    model_dir = opt.model_dir
+
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
-    if not os.path.exists(phatest_txt):
-        os.makedirs(phatest_txt)
     train_set =DatasetFromFolder(train__path,train__path)
-    training_data_loader = DataLoader(dataset=train_set, batch_size=kwargs['train_batch_size'], shuffle=kwargs['shuffle'])
+    training_data_loader = DataLoader(dataset=train_set, batch_size=opt.train_batch_size, shuffle=opt.shuffle)
 
     test_set = DatasetFromFolder(test__path, test__path)
-    testing_data_loader = DataLoader(dataset=test_set, batch_size=kwargs['test_batch_size'], shuffle=False)
+    testing_data_loader = DataLoader(dataset=test_set, batch_size=opt.test_batch_size, shuffle=False)
     G = resnet18()
     G.to(device)
-    optg = Adam(G.parameters(), lr=kwargs['learning_rate'])
+    optg = Adam(G.parameters(), lr=opt.learning_rate)
     mse = nn.MSELoss(reduction='sum')
     mse.to(device)
     writer = SummaryWriter()
     i_test = 0
 
-
-
-    for i in range(kwargs['num_steps']):
+    for i in range(opt.num_steps):
         avg_epoch_Gloss = 0
         for input_fx,input_mov, target in tqdm(training_data_loader):
             input_fx , input_mov = tonor_data(input_fx),tonor_data(input_mov)
@@ -110,16 +80,12 @@ def train(**kwargs):
         avg_epoch_Gloss = avg_epoch_Gloss / le
         writer.add_scalar('train_loss_fn', avg_epoch_Gloss, i)
         print('n_iter%d,train_loss = %f'% (i, avg_epoch_Gloss.item()))
-        if i % kwargs['display_step'] == 0 :
+        if i % opt.display_step == 0 :
             avg_loss_mse_image = 0
             for input_fx, input_mov,target in training_data_loader:
                 input_fx = input_fx.to(device)
                 input_mov = input_mov.to(device)
                 shift = G(input_fx, input_mov)
-
-                f1 = open(phat_txt+'data_%s_eoch.txt' % i, 'a')
-                f1.write('shift'+str(shift.data)+'\n'+'target'+str(target.data))
-                f1.close()
             save_file_name = os.path.join(model_dir, '%d.ckpt' % i)
             torch.save(G.state_dict(), save_file_name)
             for input_fx, input_mov, target in testing_data_loader:
@@ -131,13 +97,9 @@ def train(**kwargs):
 
                 loss_mse_image = mse(shift, target)
                 avg_loss_mse_image += loss_mse_image
-                f2 = open(phatest_txt + 'data_%s_eoch.txt' % i, 'a')
-                f2.write('shift' + str(shift.data) + '\n' + 'target' + str(target.data))
-                f2.close()
             letest = np.ceil(len(testing_data_loader.dataset))*2
             avg_loss_mse_image = avg_loss_mse_image / letest
             print("test%d , test_losse =  %f" % (i_test, avg_loss_mse_image.item()))
-            writer.add_scalar('tes_loss', avg_loss_mse_image, i_test)
             i_test += 1
     writer.close()
 
